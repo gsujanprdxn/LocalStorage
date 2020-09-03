36c4c0fa55ef82fc43df691ef3ef806cb578e1ec
gsujanprdxn <sujang@prdxn.com>
2020-09-03 14:38:30 +0530

    css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,14 @@
 
 */
 
+var itemname = document.getElementById("itemname").value;
+var itemquantity =document.getElementById("itemquantity").value;
+var forms = document.querySelector("form")
 
-
+forms.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    console.log("inside")
+})
 
 
 
